bcf9d1ebb2b1354ea2f5b57b0bd7cacc180d77b3
Tanayshri123 <tanayshri123@gmail.com>
2026-02-14 16:52:31 -0500

    minor front end polishing

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -237,6 +237,8 @@ function App() {
         <div className="bubble bubble-2"></div>
         <div className="bubble bubble-3"></div>
         <div className="bubble bubble-4"></div>
+        <div className="bubble bubble-5"></div>
+        <div className="bubble bubble-6"></div>
       </div>
       <header className="app-header">
         <h1>Aegis<span>.</span></h1>
